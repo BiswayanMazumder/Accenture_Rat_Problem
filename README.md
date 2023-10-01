a3d8e1a62128c2cb8f0acc46e5b66fda0e4773f0
BiswayanMazumder <biswayanmazumder27@gmail>
2023-10-01 22:06:03 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Accenture_Rat_Problem
+# contributions
+
+```python
+  for i in range(1, 365): # controls the number of days to commit starting from today
+    for j in range(0, randint(1, 5)): # controls the number of commits for a single day in a random number
+```
---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from random import randint
+
+for i in range(1, 5):
+    for j in range(0, randint(1, 3)):
+        d = str(i) + " days ago"
+
+        with open("file.txt", "a") as file:
+            file.write(d)
+        
+        os.system("git add .")
+        os.system('git commit --date="' + d + '" -m "commit"')
+
+os.system('git push -u origin main')
